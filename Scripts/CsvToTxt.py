3151b1401c138364ac3ff3e67b54035d6f8ead38
Dominik <dominik@rusiecki.de>
2018-05-23 20:45:53 +0200

    [ADD] csv to txt conversion template logic missing but working on it

---
+++ Scripts/CsvToTxt.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+import os
+
+template_file = "/Users/banana/PycharmProjects/TextMiningCleaning/TextMining/Scripts/template.txt"
+old_output_file = "/Users/banana/PycharmProjects/TextMiningCleaning/TextMining/Scripts/Output/output.csv"
+new_output_file = "/Users/banana/PycharmProjects/TextMiningCleaning/TextMining/Scripts/test.txt"
+
+
+
+
+
+with open(new_output_file, "w") as my_output_file:
+    with open(old_output_file, "r") as my_input_file:
+        [ my_output_file.write(" ".join(row)+'\n') for row in csv.reader(my_input_file)]
+    my_output_file.close()
